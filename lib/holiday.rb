ba1da1d13fd3d170b74a0c156b499a5d43c0cef5
w.h.sinn <w.h.sinn@gmail.com>
2019-03-23 02:47:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,7 +21,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
+def add_supply_to_winter_holidays(holiday_hash, supply_array)
   winter_supplies(supply) #=> ["Lights", "Wreath"]
 
 
