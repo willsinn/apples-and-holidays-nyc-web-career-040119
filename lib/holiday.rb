5b7d4719788965b6a3bac51feecfcdd87af324e0
w.h.sinn <w.h.sinn@gmail.com>
2019-03-23 02:48:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,9 +22,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply_array)
-  winter_supplies(supply) #=> ["Lights", "Wreath"]
-
-
+    holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end(supply) #=> ["Lights", "Wreath"]
 end
 
 
